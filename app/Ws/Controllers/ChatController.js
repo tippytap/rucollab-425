2663e2fcbaa0075ca60a7ed578861f97112b9a6c
Erik Miller <emiller46@radford.edu>
2017-04-10 21:38:23 -0400

    first stab at chatting. second connected user doesn't have any messages show up, but messages are saved and emitted from client and server

--- app/Ws/Controllers/ChatController.js
+++ app/Ws/Controllers/ChatController.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Message = use('App/Model/Message')
+const User = use('App/Model/User')
+
 class ChatController {
 
   constructor (socket, request) {
@@ -8,12 +11,20 @@ class ChatController {
   }
 
   * joinRoom(room){
-    console.log('joined ' + room)
+    // const user = this.socket.currentUser
+    console.log(room)
   }
 
-  * onMessage(message){
-    // const newMessage = yield
-    this.socket.toEveryone().inRoom('lobby').emit('message', message)
+  * onMessage(msg){
+    let message = new Message()
+    let user = yield User.find(msg.user_id)
+
+    message.user_id = msg.user_id
+    message.message_string = msg.message_string
+    yield message.save()
+    message.username = user.username
+
+    this.socket.toEveryone().inRoom('lobby').emit('message', message.toJSON())
   }
 
 }
