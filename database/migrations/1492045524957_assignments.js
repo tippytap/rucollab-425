3a3372b9e6d622d95ccae4e47a177faab320c72e
Erik Miller <emiller46@radford.edu>
2017-04-12 21:08:02 -0400

    added assignments migration

---
+++ database/migrations/1492045524957_assignments.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class AssignmentsTableSchema extends Schema {
+
+  up () {
+    this.create('assignments', (table) => {
+      table.increments()
+      table.integer('user_id').references('users.id').notNullable()
+      table.integer('task_id').references('tasks.id').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('assignments')
+  }
+
+}
+
+module.exports = AssignmentsTableSchema
