972212aec26d1cc9a8cea47d990e8fc526b7a5e8
Erik Miller <emiller46@radford.edu>
2017-04-12 21:05:04 -0400

    added tasks migration

---
+++ database/migrations/1492045298218_tasks.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class TasksTableSchema extends Schema {
+
+  up () {
+    this.create('tasks', (table) => {
+      table.increments()
+      table.string('task_string').notNullable()
+      table.date('due_date').notNullable()
+      table.dateTime('time_completed').nullable()
+      table.boolean('is_complete')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tasks')
+  }
+
+}
+
+module.exports = TasksTableSchema
